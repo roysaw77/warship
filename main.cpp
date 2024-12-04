fe340af03babab8190e06edf6d7dce4862f54555
roysaw77 <roysaw77@gmail.com>
2024-12-04 18:29:28 +0800

    map

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void map(int row,int column){
+    for(int i=0;i<row; i++){
+        for(int j=0;j<column; j++){
+          cout<<0;
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int r,c; //initial row column
+    cin>>r>>c;
+    map(r,c);
+    return 0;
+}
